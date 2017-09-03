592c8decdf2b8c2aca2fded0bf6dfdae6804890e
Kate Boston <ktlnbstn@gmail.com>
2017-09-02 21:27:22 -0700

    update state comparators.

--- src/org/launchcode/comparators/PopulationComparator.java
+++ src/org/launchcode/comparators/PopulationComparator.java
@@ -9,6 +9,6 @@ public class PopulationComparator implements Comparator<City> {
 
     @Override
     public int compare(City o1, City o2) {
-        return o1.getPopulation().compareTo(o2.getPopulation());
+        return -o1.getPopulation().compareTo(o2.getPopulation());
     }
 }
